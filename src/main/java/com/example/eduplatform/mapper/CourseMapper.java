05e906e6b92b4b577aa5e8e9d1fd642727423e0c
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-07-18 12:05:17 +0300

    feat: add CourseMapper

---
+++ src/main/java/com/example/eduplatform/mapper/CourseMapper.java
@@ -0,0 +1,22 @@
+package com.example.eduplatform.mapper;
+
+import com.example.eduplatform.dto.course.CourseCreateRequest;
+import com.example.eduplatform.dto.course.CourseResponse;
+import com.example.eduplatform.entity.Course;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CourseMapper {
+
+    Course toEntity(CourseCreateRequest courseCreateRequest);
+
+    CourseResponse toDto(Course course);
+
+    List<CourseResponse> toDto(List<Course> courseList);
+
+    void updateEntityFromDto(CourseCreateRequest courseCreateRequest, @MappingTarget Course course);
+
+}
