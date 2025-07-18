0e7275bb9f169ee353fd92153a63170d4ba38021
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-07-18 12:29:10 +0300

    feat: add CourseController

---
+++ src/main/java/com/example/eduplatform/controller/CourseController.java
@@ -0,0 +1,54 @@
+package com.example.eduplatform.controller;
+
+import com.example.eduplatform.dto.course.CourseCreateRequest;
+import com.example.eduplatform.dto.course.CourseResponse;
+import com.example.eduplatform.dto.course.CourseUpdateRequest;
+import com.example.eduplatform.service.CourseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+@RequiredArgsConstructor
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseResponse> getCourseById(@PathVariable Long id) {
+        CourseResponse course = courseService.getCourseById(id);
+        return ResponseEntity.ok(course);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> getAllCourses() {
+        List<CourseResponse> courses = courseService.getAllCourses();
+        return ResponseEntity.ok(courses);
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseResponse> createCourse(@Valid @RequestBody CourseCreateRequest courseCreateRequest) {
+        CourseResponse courseResponse = courseService.createCourse(courseCreateRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(courseResponse);
+    }
+
+    @PutMapping
+    public ResponseEntity<CourseResponse> updateCourse(@Valid @RequestBody CourseUpdateRequest courseUpdateRequest) {
+        CourseResponse courseResponse = courseService.updateCourse(courseUpdateRequest);
+        return ResponseEntity.ok(courseResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CourseResponse> deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
