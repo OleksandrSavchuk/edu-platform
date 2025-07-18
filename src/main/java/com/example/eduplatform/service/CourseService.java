3708ff0637d106946e9f553dc02632f1db4e4182
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-07-18 12:17:11 +0300

    chore: create CourseService interface and empty implementation

---
+++ src/main/java/com/example/eduplatform/service/CourseService.java
@@ -0,0 +1,21 @@
+package com.example.eduplatform.service;
+
+import com.example.eduplatform.dto.course.CourseCreateRequest;
+import com.example.eduplatform.dto.course.CourseResponse;
+import com.example.eduplatform.dto.course.CourseUpdateRequest;
+
+import java.util.List;
+
+public interface CourseService {
+
+    CourseResponse getCourseById(Long id);
+
+    List<CourseResponse> getAllCourses();
+
+    CourseResponse createCourse(CourseCreateRequest courseCreateRequest);
+
+    CourseResponse updateCourse(CourseUpdateRequest courseUpdateRequest);
+
+    void deleteCourse(Long id);
+
+}
---
+++ src/main/java/com/example/eduplatform/service/impl/CourseServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.eduplatform.service.impl;
+
+import com.example.eduplatform.dto.course.CourseCreateRequest;
+import com.example.eduplatform.dto.course.CourseResponse;
+import com.example.eduplatform.dto.course.CourseUpdateRequest;
+import com.example.eduplatform.repository.CourseRepository;
+import com.example.eduplatform.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Override
+    public CourseResponse getCourseById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CourseResponse> getAllCourses() {
+        return List.of();
+    }
+
+    @Override
+    public CourseResponse createCourse(CourseCreateRequest courseCreateRequest) {
+        return null;
+    }
+
+    @Override
+    public CourseResponse updateCourse(CourseUpdateRequest courseUpdateRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteCourse(Long id) {
+
+    }
+
+}
