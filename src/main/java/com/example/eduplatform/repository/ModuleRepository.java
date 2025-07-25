0b8b79f1fb513d4989b1668acdff8ccb66c7feb2
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-07-25 22:48:58 +0300

    feat: implement all methods for ModuleService

--- src/main/java/com/example/eduplatform/repository/ModuleRepository.java
+++ src/main/java/com/example/eduplatform/repository/ModuleRepository.java
@@ -1,11 +1,16 @@
 package com.example.eduplatform.repository;
 
+import com.example.eduplatform.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 
-    public boolean existsByIdAndCourseId(Long moduleId, Long courseId);
+    boolean existsByIdAndCourseId(Long moduleId, Long courseId);
+
+    List<Module> findAllByCourseId(Long courseId);
 
 }
--- src/main/java/com/example/eduplatform/service/impl/ModuleServiceImpl.java
+++ src/main/java/com/example/eduplatform/service/impl/ModuleServiceImpl.java
@@ -3,13 +3,17 @@ package com.example.eduplatform.service.impl;
 import com.example.eduplatform.dto.module.ModuleCreateRequest;
 import com.example.eduplatform.dto.module.ModuleResponse;
 import com.example.eduplatform.dto.module.ModuleUpdateRequest;
+import com.example.eduplatform.entity.Course;
 import com.example.eduplatform.entity.Module;
+import com.example.eduplatform.exception.ResourceNotFoundException;
 import com.example.eduplatform.mapper.ModuleMapper;
 import com.example.eduplatform.repository.ModuleRepository;
+import com.example.eduplatform.service.CourseService;
 import com.example.eduplatform.service.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,42 +21,58 @@ import java.util.List;
 public class ModuleServiceImpl implements ModuleService {
 
     private final ModuleRepository moduleRepository;
-
     private final ModuleMapper moduleMapper;
+    private final CourseService courseService;
 
     @Override
     public ModuleResponse getById(Long id) {
-        return null;
+        Module module = moduleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Module with id " + id + " not found"));
+        return moduleMapper.toDto(module);
     }
 
     @Override
     public Module getModuleById(Long id) {
-        return null;
+        return moduleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Module with id " + id + " not found"));
     }
 
     @Override
     public List<ModuleResponse> getAllModules(Long courseId) {
-        return List.of();
+        List<Module> modules = moduleRepository.findAllByCourseId(courseId);
+        return moduleMapper.toDto(modules);
     }
 
     @Override
     public ModuleResponse createModule(Long courseId, ModuleCreateRequest moduleCreateRequest) {
-        return null;
+        Course course = courseService.getCourseById(courseId);
+        Module module = moduleMapper.toEntity(moduleCreateRequest);
+        module.setCreatedAt(LocalDateTime.now());
+        module.setUpdatedAt(LocalDateTime.now());
+        module.setCourse(course);
+        return moduleMapper.toDto(moduleRepository.save(module));
     }
 
     @Override
     public ModuleResponse updateModule(Long id, ModuleUpdateRequest moduleUpdateRequest) {
-        return null;
+        Module module = moduleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Module with id " + id + " not found"));
+        moduleMapper.updateEntityFromDto(moduleUpdateRequest, module);
+        module.setUpdatedAt(LocalDateTime.now());
+        return moduleMapper.toDto(moduleRepository.save(module));
     }
 
     @Override
     public void deleteModule(Long id) {
-
+        if (!moduleRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Module with id " + id + " not found");
+        }
+        moduleRepository.deleteById(id);
     }
 
     @Override
     public boolean existsByIdAndCourseId(Long moduleId, Long courseId) {
-        return false;
+        return moduleRepository.existsByIdAndCourseId(moduleId, courseId);
     }
 
 }
