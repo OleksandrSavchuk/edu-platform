c02a00dd4c1b96ac562bfbc34bf4954aa56dfc41
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-07-28 18:32:23 +0300

    feat: add EnrollmentMapper

---
+++ src/main/java/com/example/eduplatform/mapper/EnrollmentMapper.java
@@ -0,0 +1,19 @@
+package com.example.eduplatform.mapper;
+
+import com.example.eduplatform.dto.enrollment.EnrollmentCreateRequest;
+import com.example.eduplatform.dto.enrollment.EnrollmentResponse;
+import com.example.eduplatform.entity.Enrollment;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface EnrollmentMapper {
+
+    Enrollment toEntity(EnrollmentCreateRequest enrollmentCreateRequest);
+
+    EnrollmentResponse toDto(Enrollment enrollment);
+
+    List<EnrollmentResponse> toDto(List<Enrollment> enrollments);
+
+}
