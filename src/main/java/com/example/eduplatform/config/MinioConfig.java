4e9690862d0806dbbb58996f9fe4b02f73008d14
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-08-11 13:49:30 +0300

    feat: add MinioConfig class for MinIO client setup

---
+++ src/main/java/com/example/eduplatform/config/MinioConfig.java
@@ -0,0 +1,23 @@
+package com.example.eduplatform.config;
+
+import com.example.eduplatform.props.MinioProperties;
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MinioConfig {
+
+    private final MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getUrl())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+
+}
