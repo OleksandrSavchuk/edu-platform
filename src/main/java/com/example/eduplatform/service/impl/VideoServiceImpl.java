e0719034d83e7c91a9c507be1d819909873f6d50
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-08-15 14:35:42 +0300

    feat: add implementation for VideoService with MinIO video upload

--- src/main/java/com/example/eduplatform/service/impl/VideoServiceImpl.java
+++ src/main/java/com/example/eduplatform/service/impl/VideoServiceImpl.java
@@ -1,11 +1,19 @@
 package com.example.eduplatform.service.impl;
 
 import com.example.eduplatform.dto.lesson.LessonVideo;
+import com.example.eduplatform.exception.VideoUploadException;
 import com.example.eduplatform.props.MinioProperties;
 import com.example.eduplatform.service.VideoService;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +24,56 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public String upload(LessonVideo video) {
-        return "";
+        MultipartFile file = video.getFile();
+        validateVideo(file);
+
+        String fileName = generateFileName(file);
+        try {
+            createBucket();
+            saveVideo(file, fileName);
+        } catch (Exception e) {
+            throw new VideoUploadException("Video upload failed: " + e.getMessage());
+        }
+        return fileName;
     }
 
+    private void validateVideo(MultipartFile file) {
+        if (file.isEmpty() || file.getOriginalFilename().trim().isEmpty()) {
+            throw new VideoUploadException("Video must have a name");
+        }
+
+    }
+
+    private void createBucket() throws Exception {
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder()
+                .bucket(minioProperties.getBucket())
+                .build());
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder()
+                    .bucket(minioProperties.getBucket())
+                    .build());
+        }
+    }
+
+    private String generateFileName(MultipartFile file) {
+        return UUID.randomUUID() + "." + getExtension(file);
+    }
+
+    private String getExtension(MultipartFile file) {
+        String name = file.getOriginalFilename();
+        if (name.contains(".")) {
+            return name.substring(name.lastIndexOf('.') + 1);
+        }
+        throw new VideoUploadException("Invalid file format");
+    }
+
+    private void saveVideo(MultipartFile file, String filename) throws Exception {
+        try (InputStream inputStream = file.getInputStream()) {
+            minioClient.putObject(PutObjectArgs.builder()
+                    .stream(inputStream, file.getSize(), -1)
+                    .bucket(minioProperties.getBucket())
+                    .object(filename)
+                    .build());
+        }
+    }
 }
