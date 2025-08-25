d6d981981b56fd5c36783296a6233b65b22a9509
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-08-25 20:50:39 +0300

    fix: add @PreAuthorize annotations to UserResolver

--- src/main/java/com/example/eduplatform/resolver/UserResolver.java
+++ src/main/java/com/example/eduplatform/resolver/UserResolver.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -18,21 +19,25 @@ public class UserResolver {
     private final UserService userService;
 
     @QueryMapping
+    @PreAuthorize("hasRole('ADMIN') or @customSecurityExpression.canAccessUser(#id)")
     public UserResponse getUserById(@Argument Long id) {
         return userService.getById(id);
     }
 
     @QueryMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @MutationMapping
+    @PreAuthorize("@customSecurityExpression.canAccessUser(#id)")
     public UserResponse updateUser(@Argument Long id, @Argument UserUpdateRequest userUpdateRequest) {
         return userService.updateUser(id, userUpdateRequest);
     }
 
     @MutationMapping
+    @PreAuthorize("@customSecurityExpression.canAccessUser(#id)")
     public void deleteUser(@Argument Long id) {
         userService.deleteUser(id);
     }
